0d32da0c6996817c8020c2090191c7ed85938bfc
mpedgar467 <mpedgar467@g.educaand.es>
2025-03-21 12:38:50 +0100

    modificación

--- ex_html-css-js.js
+++ ex_html-css-js.js
@@ -10,24 +10,24 @@
 <body>
     <h1>¡¡Pide aquí tu rico pescaíto!!</h1>
 
-    <img src="pescado_frito.jpg" alt="Bandeja pescado frito">
-    <form action="enviar_pedido.php" method="POST">
-        <h2>Datos del cliente</h2>
+    <img src="pescado_frito.jpg" <h2></h2>="Bandeja pescado frito"</h2>
+    <form action="enviar_pedido.php"
+        <h3>Datos del cliente</h3>
         
         <label for="nombre">Nombre: </label>
-        <input type="text" id="nombre" name="nombre" required><br><br>
+        <input type="text" id="nombre" name="nombre" required>
 
         <label for="apellidos">Apellidos: </label>
-        <input type="text" id="apellidos" name="apellidos" required><br><br>
+        <input type="text" id="apellidos" name="apellidos" required>
 
         <label for="email">Email: </label>
-        <input type="email" id="email" name="email" required><br><br>
+        <input type="email" id="email" name="email" required>
 
         <label for="direccion">Dirección (mínimo 18 caracteres): </label>
-        <input type="text" id="direccion" name="direccion" minlength="18" required><br><br>
+        <input type="text" id="direccion" name="direccion" minlength="18" required>
 
         <label for="telefono">Teléfono (9 dígitos): </label>
-        <input type="tel" id="telefono" name="telefono" pattern="{9}" required><br><br>
+        <input type="tel" id="telefono" name="telefono" pattern="{9}" required>
 
         <label for="hora">Hora aproximada de entrega (HH:MM): </label>
 
